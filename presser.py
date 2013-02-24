a08872cdc585d928116d6087e9b78e120eae62c8
Alonisser <alonisser@gmail.com>
2013-02-25 00:26:17 +0200

    first version of presser, reads from commandline and config file

--- presser.py
+++ presser.py
@@ -1,9 +1,34 @@
-#python
+#!/usr/bin/env python
+from __future__ import print_function
+from ConfigParser import SafeConfigParser
+from markdown import markdown
+import sys
+import requests
 
 #parse some commandline arguments
+if sys.argv[1]==None:
+    exit()
+
+input = sys.argv[1]
+
+with open(input,"r") as f:
+    html = markdown(f.read())
+    f.close()
+
+with open('output.html','w') as f:
+    f.write(html)
+    f.close()
+    print("written: %s" % 'output.html')
 
 #parse configfile to wordpress
+parser = SafeConfigParser()
+parser.read('config.ini')
 
+siteurl = parser.get('wordpress_site','site')
+username = parser.get('wordpress_site','username')
+password = parser.get('wordpress_site','password')
+
+print (siteurl, username, password, sep="\n")
 #parse the markdown file to html
 
 #send the parsed html to wordpress
