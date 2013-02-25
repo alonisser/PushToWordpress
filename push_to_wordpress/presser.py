c32c65438c6a885b18ebd71d8f2b21b781f76c97
Alonisser <alonisser@gmail.com>
2013-02-26 01:48:24 +0200

    commandline script now working with setuptools entry point, another refactoring of project structure

--- push_to_wordpress/presser.py
+++ push_to_wordpress/presser.py
@@ -3,45 +3,51 @@ from __future__ import print_function
 from ConfigParser import SafeConfigParser
 from markdown import markdown
 import sys
+import os
 from wordpress_xmlrpc import WordPressPost, Client
 from wordpress_xmlrpc.methods.posts import NewPost
 
-#TODO: change this part to optionparser
-if sys.argv[1]==None:
-    print("missing input file")
-    exit()
+def presser():
+    #TODO: change this part to optionparser
+    if len(sys.argv) == 1 or sys.argv[1] == None:
+        print ("missing input file")
+        exit()
 
-input = sys.argv[1]
+    input = sys.argv[1]
 
-with open(input,"r") as f:
-    #TODO: add try block to check for encoded strings or files and then use codecs to open
-    html = markdown(f.read())
-    f.close()
-#parse the markdown file to html
-# with open('output.html','w') as f:
-#     f.write(html)
-#     f.close()
-#     print("written: %s" % 'output.html')
+    with open(input,"r") as f:
+        #TODO: add try block to check for encoded strings or files and then use codecs to open
+        html = markdown(f.read())
+        f.close()
+    #parse the markdown file to html
+    # with open('output.html','w') as f:
+    #     f.write(html)
+    #     f.close()
+    #     print("written: %s" % 'output.html')
 
-#parse configfile to wordpress
-#TODO: add an option to use commandline args instead of config file
-parser = SafeConfigParser()
-parser.read('config.ini')
+    #parse configfile to wordpress
+    #TODO: add an option to use commandline args instead of config file
+    parser = SafeConfigParser()
+    configpath = os.path.dirname(os.path.abspath("__file__"))
+    parser.read(os.path.join(configpath,'config.ini'))
 
-siteurl = "%s/%s" % (parser.get('wordpress_site','site'),"xmlrpc.php")
-username = parser.get('wordpress_site','username')
-password = parser.get('wordpress_site','password')
-#TODO: check for missing configs
+    siteurl = "%s/%s" % (parser.get('wordpress_site','site'),"xmlrpc.php")
+    username = parser.get('wordpress_site','username')
+    password = parser.get('wordpress_site','password')
+    #TODO: check for missing configs
 
-print (siteurl, username, password, sep="\n")
-#send the parsed html to wordpress
-wp = Client(siteurl,username, password)
-post = WordPressPost()
-if sys.argv[2]:
-    post.title = sys.argv[2]
-else:
-    post.title = 'A new draft post'
-post.content = html
-post.post_status = 'draft'
+    print ('connection settings:', siteurl, username, password, sep="\n")
+    #send the parsed html to wordpress
+    wp = Client(siteurl,username, password)
+    post = WordPressPost()
+    try:
+        post.title = sys.argv[2]
+    except IndexError:
+        post.title = 'A new draft post'
+    post.content = html
+    post.post_status = 'draft'
 
-wp.call(NewPost(post))
+    wp.call(NewPost(post))
+
+if __name__ == '__main__':
+   presser()
--- setup.py
+++ setup.py
@@ -23,17 +23,20 @@ if sys.argv[-1] == 'publish':
 setup(
     name = "Push_To_Wordpress",
     version = VERSION,
-    scripts = ['presser.py'],
+    #cripts = ['presser.py'],
 
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
     install_requires = required,
 
     package_data = {
         # If any package contains *.txt or *.rst files, include them:
         '': ['*.md', '*.rst'],
-    }
-
+    },
+    packages = ['push_to_wordpress'],
+    entry_points = {
+        'console_scripts': [
+            'presser = push_to_wordpress.presser:presser'
+        ],
+    },
     # metadata for upload to PyPI
     author = "Alonisser",
     author_email = "alonisser@gmail.com",
@@ -41,6 +44,16 @@ setup(
     license = "permissive",
     keywords = "blogging wordpress commandline",
     url = "https://github.com/alonisser/PushToWordpress",   # project home page, if any
+    classifiers=(
+        'Development Status :: 1 - alpha',
+        'Intended Audience :: bloggers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+#maybe more, didn't check
+    ),
 
     # could also include long_description, download_url, classifiers, etc.
 )
