1e36525b7549b0b010ee93044fb194a16d5fbd0c
Alonisser <alonisser@gmail.com>
2013-07-05 15:41:16 +0300

    added a dry run mode, added validation and error handling for correct credentials and url and various other error. making this more rubust

--- push_to_wordpress/presser.py
+++ push_to_wordpress/presser.py
@@ -3,10 +3,12 @@ from __future__ import print_function
 from ConfigParser import SafeConfigParser
 from ConfigParser import NoSectionError, NoOptionError
 from markdown import markdown
-import os
+import os, sys
 from wordpress_xmlrpc import WordPressPost, Client
 from wordpress_xmlrpc.methods.posts import NewPost
+from wordpress_xmlrpc.exceptions import InvalidCredentialsError, ServerConnectionError
 import argparse
+import re
 
 def read_config(configuration_parser):
     cparser = configuration_parser
@@ -22,37 +24,54 @@ def presser():
     parser.add_argument('--title', action='store', dest='title', default='A new post' )
     parser.add_argument('--posts', help='a space seperated list of post names', default=[], dest='posts_to_process',nargs='+')
     parser.add_argument('--status', choices=('draft', 'publish','private'),help='post status defaults to %(default)s', default='draft', dest='status')
-    
-    # parser.add_argument('--siturl', action='store', dest='siteurl', help="the siteurl without xmlrpc overrides the config file" )
-    # parser.add_argument('--username', action='store', dest='username', help="the username overrides the config file")
-    # parser.add_argument('--password', action='store', dest='password', help="the password overrides the config file")
-    # parser.add_argument('help')#to implent later
-    parser.add_argument('--dry-run',action="store_true", default=False) #to implent later
+    parser.add_argument('--dry-run',action="store_true", dest = 'dry_run', default=False) #to implent later
+    ## need to implent a fuller Usage description
     options = parser.parse_args()
-    
+
+    exiting = "Would exit now. run presser --help for commandline options"
     #parse configfile to wordpress
     #TODO: add an option to use commandline args instead of config file
     cparser = SafeConfigParser()
-    try: #if there is a config options
+    try: #if there is a config option in commandline options
         cparser.read(options.configfile)
         siteurl, username, password = read_config(cparser)
     except TypeError:
         try:
+            #searching for configfile in local folder. TODO: change this to a recursive tree search
             configpath = os.path.dirname(os.path.abspath("__file__"))
             cparser.read(os.path.join(configpath,'config.ini'))
             siteurl, username, password = read_config(cparser)
         except (NoSectionError, NoOptionError):
-            print("missing configuration information or file")
+            print("missing configuration information or file, please input them now:")
             import getpass
             siteurl = '%s/%s' % (raw_input("Full site url: "),"xmlrpc.php")
             username = raw_input("Wordpress site username: ")
             password = getpass.getpass("Enter wordpress site password: ")
     
+    #very basic, checking for http:// in the beginning, since this fails wordpress_xmprpc
+    valid_url = re.match(r"^http://", siteurl)
+    
+    if not valid_url:
+        siteurl = "".join(["http://", siteurl])
     # TODO: check for missing configs
 
-    print ('connection settings:', siteurl, username, password, sep="\n")
+    print ('connection settings:',"Site url: %s" % siteurl, "User name: %s" % username, sep="\n")
+    
+    #create a connection
+    try:
+        wp = Client(siteurl,username, password)
+    except (IOError, ServerConnectionError) as e:
+        print("Either a invalid wordpress site address or your site doesn't have the XML-RPC protocol turned on.", 
+        exiting, sep="\n")
+        sys.exit()
+    
+    if options.dry_run:
+        print("Running a dry run, won't save any posts.")
+
     #send the parsed html to wordpress
-    wp = Client(siteurl,username, password)
+    if len(options.posts_to_process) == 0:
+        print("No posts to post",exiting ,sep="\n")
+        sys.exit()
 
     for p in options.posts_to_process:
         with open(p,"r") as f:
@@ -64,9 +83,17 @@ def presser():
         post.title =options.title
         post.content = html
         post.post_status = options.status
-
-        wp.call(NewPost(post))
-        print ('Done uploading: %s' % p)
+        if options.dry_run:
+            print("Dry run mode:")
+            print("Post name: %s.\n Post content:%s\n Post status:%s" % (post.title, post.content, post.post_status))
+            print("If you really want to post try running not in 'dry run' mode")
+            continue
+        try:
+            wp.call(NewPost(post))
+            print ('Done uploading: %s' % p)
+        except InvalidCredentialsError as e:
+            print(e.message, exiting, sep="\n")
+            sys.exit()
 
 if __name__ == '__main__':
    presser()
